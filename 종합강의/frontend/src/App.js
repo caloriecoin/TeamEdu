a86ab38a47dd86270b3a20d3a847afd02aa5d751
BAEK <100k1@naver.com>
2022-10-26 18:13:15 +0900

    종합강의 푸쉬

--- 종합강의/frontend/src/App.js
+++ 종합강의/frontend/src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [name, 이름옮겨] = useState(null);
+  const [age, 나이옮겨] = useState(null);
+  const [years, 연도옮겨] = useState(null);
+
+  Axios.get("http://192.168.0.31:5000/ceo").then(function (소현컴응답) {
+    이름옮겨(소현컴응답.data.name);
+    나이옮겨(소현컴응답.data.age);
+    연도옮겨(소현컴응답.data.years);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {name}씨는 {age}살 {years}입니다.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        ></a>
       </header>
     </div>
   );
